2f5bf572939b7414504d359d382fd6fe3081831b
Ovidiu Cota <ovidiu.cota@pitechnologies.ro>
2017-04-03 19:19:17 +0300

    some configs

---
+++ app/assets/javascripts/greet.jsx
@@ -0,0 +1,7 @@
+class Greet extends React.Component {
+  render() {
+    return (
+      <h2>Hello There</h2>
+    );
+  }
+}
---
+++ app/assets/javascripts/main.jsx
@@ -0,0 +1,16 @@
+class Main extends React.Component {
+  render() {
+    return (
+      <Greet />
+    );
+  }
+}
+
+let documentReady = () => {
+  React.render(
+    <Main />,
+    document.getElementById('react')
+  );
+};
+
+$(documentReady);
